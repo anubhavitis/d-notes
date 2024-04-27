32be51c2da68bca3c9405d0497519d343e940f4d
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-04-27 20:00:18 +0530

    feat: del note added

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const gun = Gun({
 });
 const user = gun.user().recall({ sessionStorage: true });
 
+
 let signup = document.getElementById('sign-up');
 let signin = document.getElementById('sign-in');
-let said = document.getElementById('said');
+let addNotes = document.getElementById('notes-form');
 let signout = document.getElementById('sign-out');
+
+let password = document.getElementById('password');
+let username = document.getElementById('username');
+
+let newMessage = document.getElementById('new-message');
 
 signout.addEventListener('click', function (e) {
     sessionStorage.clear();
@@ -14,24 +20,21 @@ signout.addEventListener('click', function (e) {
 });
 
 signup.addEventListener('click', function (e) {
-    user.create(document.getElementById('alias').value, document.getElementById('pass').value);
+    user.create(username.value, password.value);
 });
 
 signin.addEventListener('submit', function (e) {
     e.preventDefault();
-    user.auth(document.getElementById('alias').value, document.getElementById('pass').value);
+    user.auth(username.value, password.value);
 });
 
-said.addEventListener('submit', function (e) {
-    e.preventDefault();
-    if (!user.is) { return; }
-    user.get('said').set(document.getElementById('say').value);
-    document.getElementById('say').value = "";
-});
+addNotes.addEventListener('submit', addItem);
 
-function UI(say, id) {
+function notesItems(say, id) {
+    console.log("say", say, "id", id);
+    if (!say) { return; }
     let item = document.createElement('div');
-    item.classList.add('flex', 'justify-start', 'items-center', 'mb-2', 'gap-2', 'w-full');
+    item.classList.add('flex', 'justify-start', 'items-center', 'mb-2', 'gap-2', 'w-full', 'border', 'rounded', 'hover:shadow-lg', 'py-2', 'px-4');
 
     let li = document.getElementById(id) || document.createElement('li');
     li.id = id;
@@ -39,15 +42,66 @@ function UI(say, id) {
     li.textContent = say;
 
     let del = document.createElement('button');
-    del.classList.add('bg-red-400', 'text-white', 'px-2', 'py-1', 'rounded-md', 'hover:bg-red-600');
-    del.innerHTML = 'X';
+    del.classList.add('bg-red-400', 'text-white', 'p-2', 'rounded-md', 'hover:bg-red-800');
+    del.onclick = function (e) {
+        e.preventDefault();
+        removeItem(id);
+
+        // create pause for 500ms to let the data sync
+        setTimeout(() => {
+            buildNotes();
+        }, 200);
+    }
+    del.innerHTML = 'x';
 
     item.appendChild(del);
     item.appendChild(li);
     document.querySelector('ul').appendChild(item);
 }
 
+function buildNotes() {
+    console.log("building notes");
+    document.querySelector('ul').innerHTML = "";
+
+    // Here once() is attaching callback func of notesItems()
+    // so to every node in said, notesItems() will be attached and called while creation
+    user.get('said').map().once(notesItems);
+}
+
 gun.on('auth', function () {
+    console.log("I am getting called");
     signin.style.display = 'none';
-    user.get('said').map().once(UI);
+    buildNotes();
 });
+
+window.onload = function () {
+    console.log("hello, world!")
+}
+
+function addItem(e) {
+    e.preventDefault();
+    let val = newMessage.value;
+    if (val.trim() === "") {
+        newMessage.value = "";
+        window.alert("Please enter a message")
+        return;
+    }
+
+    if (!user.is) { return; }
+    user.get('said').set(newMessage.value);
+    newMessage.value = "";
+}
+
+function removeItem(id) {
+
+    console.log("item to remove:", id);
+    let node = user.get('said').get(id);
+    console.log("all said:", node);
+
+    // unset should work, but its not working
+    // user.get('said').unset(node)
+
+    // moving ahead with marking it null, and filtering it out in UI.
+    node.put(null);
+    // TODO: find a better way to remove the item
+}
