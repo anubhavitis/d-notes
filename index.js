21a2c844baaf17793c721f58d758303b6a6adb15
Anubhav Singhal <ss.lfsgd@gmail.com>
2024-04-27 16:18:01 +0530

    feat: another tailwind magic. Urgh

--- index.js
+++ index.js
@@ -20,11 +20,21 @@ document.getElementById('said').addEventListener('submit', function (e) {
 });
 
 function UI(say, id) {
+    let item = document.createElement('div');
+    item.classList.add('flex', 'justify-start', 'items-center', 'mb-2', 'gap-2', 'w-full');
+
     let li = document.getElementById(id) || document.createElement('li');
     li.id = id;
     li.classList.add('bg-blue-200', 'text-blue-800', 'px-4', 'py-2', 'rounded-md');
     li.textContent = say;
-    document.querySelector('ul').appendChild(li);
+
+    let del = document.createElement('button');
+    del.classList.add('bg-red-400', 'text-white', 'px-2', 'py-1', 'rounded-md', 'hover:bg-red-600');
+    del.innerHTML = 'X';
+
+    item.appendChild(del);
+    item.appendChild(li);
+    document.querySelector('ul').appendChild(item);
 }
 
 gun.on('auth', function () {
